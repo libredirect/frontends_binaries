1770fa71df2c085425e9d6eb5f3227abd1c1a45b
ManeraKai <manerakai@protonmail.com>
2023-05-22 11:28:50 +0300

    Now added settings.json

--- stdin_parser.sh
+++ stdin_parser.sh
@@ -3,8 +3,8 @@ GOARCH=amd64 GOOS=linux   go build main.go && mv main     stdin_parser_linux_x86
 GOARCH=amd64 GOOS=windows go build main.go && mv main.exe stdin_parser_windows_x86_64.exe
 cd ../
 
-tar -czvf stdin_parser_linux_x86_64.tar.gz stdin_parser/stdin_parser_linux_x86_64 stdin_parser/org.libredirect.stdin_parser.json
-zip       stdin_parser_windows_x86_64.zip  stdin_parser/stdin_parser_windows_x86_64.exe stdin_parser/org.libredirect.stdin_parser.json
+tar -czvf stdin_parser_linux_x86_64.tar.gz stdin_parser/stdin_parser_linux_x86_64 stdin_parser/org.libredirect.stdin_parser.json stdin_parser/settings.json
+zip       stdin_parser_windows_x86_64.zip  stdin_parser/stdin_parser_windows_x86_64.exe stdin_parser/org.libredirect.stdin_parser.json stdin_parser/settings.json
 
 mv stdin_parser_linux_x86_64.tar.gz binaries/
 mv stdin_parser_windows_x86_64.zip  binaries/
